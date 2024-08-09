0a05c34d9e235496464e2c11ab26f438c55e5251
Eesa Zahed <eszhd1@gmail.com>
2024-08-09 13:14:01 -0400

    added gradient and link to 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Gradient from "../components/Gradient";
+import Link from "../components/Link";
 
 const Custom404: NextPage = () => {
   return (
@@ -14,11 +16,16 @@ const Custom404: NextPage = () => {
       </Head>
 
       <main className="py-16 leading-8 text-xl grid min-h-screen place-items-center">
-        <h1 className="my-8 text-7xl text-center font-bold">Page not found</h1>
+        <h1 className="my-8 text-6xl md:text-8xl text-center font-bold">
+          <Gradient text="Page not found" /> 😭
+        </h1>
       </main>
 
       <footer className="py-16 text-center">
-        <p>Designed and developed by Eesa Zahed</p>
+        <p>
+          Designed and developed by{" "}
+          <Link text="Eesa Zahed" href="https://eesa.zahed.ca" />
+        </p>
       </footer>
     </div>
   );
